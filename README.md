664edfb600cb91cfc901a74547e360e9d06565a2
Trigus42 <59501676+Trigus42@users.noreply.github.com>
2021-08-22 16:57:08 +0200

    See commit description
    - Don't add fwmark by default; Add USE_FWMARK environment variable
    - Improve log messages
    - Update README

--- README.md
+++ README.md
@@ -93,22 +93,24 @@ $ docker run --privileged -d \
 ### Environment Variables
 | Variable | Required | Function | Example | Default |
 |----------|----------|----------|----------|----------|
-|`UNPRIVILEGED`| No | Allows container to run in unprivileged mode when wireguard is used |`UNPRIVILEGED=yes`|`no`|
 |`VPN_ENABLED`| Yes | Enable VPN (yes/no)?|`VPN_ENABLED=yes`|`yes`|
 |`VPN_TYPE`| Yes | WireGuard or OpenVPN (wireguard/openvpn)?|`VPN_TYPE=openvpn`|`wireguard`|
+|`UNPRIVILEGED`| No | Allows container to run in unprivileged mode when wireguard is used |`UNPRIVILEGED=yes`|`no`|
 |`VPN_USERNAME`| No | If username and password provided, configures ovpn file automatically |`VPN_USERNAME=ad8f64c02a2de`||
 |`VPN_PASSWORD`| No | If username and password provided, configures ovpn file automatically |`VPN_PASSWORD=ac98df79ed7fb`||
 |`LAN_NETWORK`| Yes (atleast one) | Comma delimited local Network's with CIDR notation |`LAN_NETWORK=192.168.0.0/24,10.10.0.0/24`||
+|`SET_FWMARK`| No | Make web interface reachable for devices in networks not specified in `LAN_NETWORK`. (This is known to break easily) |`yes`|`no`|
 |`ENABLE_SSL`| No | Let the container handle SSL (yes/no)? |`ENABLE_SSL=yes`|`yes`|
 |`NAME_SERVERS`| No | Comma delimited name servers |`NAME_SERVERS=1.1.1.1,1.0.0.1`|`1.1.1.1,1.0.0.1`|
 |`PUID`| No | UID applied to /config files and /downloads |`PUID=99`|`99`|
 |`PGID`| No | GID applied to /config files and /downloads  |`PGID=100`|`100`|
 |`UMASK`| No | |`UMASK=002`|`002`|
-|`HEALTH_CHECK_HOST`| No |This is the host or IP that the healthcheck script will use to check an active connection|`HEALTH_CHECK_HOST=one.one.one.one`|`one.one.one.one`|
-|`HEALTH_CHECK_INTERVAL`| No |This is the time in seconds that the container waits to see if the internet connection still works (check if VPN died)|`HEALTH_CHECK_INTERVAL=300`|`300`|
-|`HEALTH_CHECK_SILENT`| No |Set to `1` to supress the 'Network is up' message. Defaults to `1` if unset.|`HEALTH_CHECK_SILENT=1`|`1`|
-|`INSTALL_PYTHON3`| No |Set this to `yes` to let the container install Python3.|`INSTALL_PYTHON3=yes`|`no`|
-|`ADDITIONAL_PORTS`| No |Adding a comma delimited list of ports will allow these ports via the iptables script.|`ADDITIONAL_PORTS=1234,8112`||
+|`HEALTH_CHECK_HOST`| No | This is the host or IP that the healthcheck script will use to check an active connection |`HEALTH_CHECK_HOST=one.one.one.one`|`one.one.one.one`|
+|`HEALTH_CHECK_INTERVAL`| No | This is the time in seconds that the container waits to see if the internet connection still works (check if VPN died) |`HEALTH_CHECK_INTERVAL=300`|`300`|
+|`HEALTH_CHECK_SILENT`| No | Set to `1` to supress the 'Network is up' message. Defaults to `1` if unset |`HEALTH_CHECK_SILENT=1`|`1`|
+|`INSTALL_PYTHON3`| No | Set this to `yes` to let the container install Python3 |`INSTALL_PYTHON3=yes`|`no`|
+|`ADDITIONAL_PORTS`| No | Adding a comma delimited list of ports will allow these ports via the iptables script |`ADDITIONAL_PORTS=1234,8112`||
+|`DEBUG`| No | Print information useful for debugging in log |`yes`|`no`|
 
 ### Volumes
 | Volume | Required | Function | Example |
--- init/iptables.sh
+++ init/iptables.sh
@@ -12,19 +12,19 @@ done
 
 # identify docker bridge interface name (probably eth0)
 docker_interface="$(netstat -ie | grep -vE "lo|tun|tap|wg" | sed -n '1!p' | grep -P -o -m 1 '^[\w]+')"
-if [[ "${DEBUG}" == "true" ]]; then
+if [[ "${DEBUG}" == "yes" ]]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] Docker interface defined as ${docker_interface}"
 fi
 
 # identify ip for docker bridge interface
 docker_ip="$(ip -4 addr show "${docker_interface}" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
-if [[ "${DEBUG}" == "true" ]]; then
+if [[ "${DEBUG}" == "yes" ]]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] Docker IP defined as ${docker_ip}"
 fi
 
 # identify netmask for docker bridge interface
 docker_mask=$(ifconfig "${docker_interface}" | grep -o "Mask:[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-if [[ "${DEBUG}" == "true" ]]; then
+if [[ "${DEBUG}" == "yes" ]]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] Docker netmask defined as ${docker_mask}"
 fi
 
@@ -38,6 +38,13 @@ echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] Docker network defined as ${docker_net
 # get default gateway of interfaces as looping through them
 DEFAULT_GATEWAY=$(ip -4 route list 0/0 | cut -d ' ' -f 3)
 
+if [[ "${DEBUG}" == "yes" ]]; then
+	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] Modules currently loaded for kernel:"
+	echo "--------------------"
+	lsmod
+	echo "--------------------"
+fi
+
 # split comma separated string into list from LAN_NETWORK env variable
 IFS=',' read -ra lan_network_list <<< "${LAN_NETWORK}"
 
@@ -47,34 +54,52 @@ for lan_network_item in "${lan_network_list[@]}"; do
 	lan_network_item=$(echo "${lan_network_item}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
 
 	echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] Adding ${lan_network_item} as route via docker ${docker_interface}" 
-	ip route add "${lan_network_item}" via "${DEFAULT_GATEWAY}" dev "${docker_interface}"
+	ip route add "${lan_network_item}" via "${DEFAULT_GATEWAY}" dev "${docker_interface}" &> /dev/null
+
+	ip_route_add_exit_code=$?
+
+	if [[ $ip_route_add_exit_code != 0 && $SET_FWMARK == "yes" ]]; then
+		echo "$(date +'%Y-%m-%d %H:%M:%S') [WARNING] Error adding route for $lan_network_item. The web interface will still be reachable due to fwmark. However this is known to cause issues."
+	elif [[ $ip_route_add_exit_code != 0 ]]; then
+		echo "$(date +'%Y-%m-%d %H:%M:%S') [WARNING] Error adding route for $lan_network_item. The web interface won't be reachable for the affected network"
+	fi
 done
 
-echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] ip route defined as follows..."
-echo "--------------------"
-ip route
-echo "--------------------"
-
-# setup iptables marks to allow routing of defined ports via "${docker_interface}"
-###
-
-if [[ "${DEBUG}" == "true" ]]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] Modules currently loaded for kernel"
-	lsmod
-fi
+## Setup iptables marks to allow routing of defined ports via "${docker_interface}"
 
 # check we have iptable_mangle, if so setup fwmark
-lsmod | grep iptable_mangle
+lsmod | grep iptable_mangle &> /dev/null
 iptable_mangle_exit_code=$?
 
 if [[ $iptable_mangle_exit_code == 0 ]]; then
-	echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] iptable_mangle support detected, adding fwmark for tables"
+	if [[ $SET_FWMARK == "yes" ]]; then
+		echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] Adding fwmark for webui."
 
-	# setup route for qBittorrent webui using set-mark to route traffic for port 8080 and 8999 to "${docker_interface}"
-	echo "8080    webui" >> /etc/iproute2/rt_tables
-	echo "8999    webui" >> /etc/iproute2/rt_tables
-	ip rule add fwmark 1 table webui
-	ip route add default via "${DEFAULT_GATEWAY}" table webui
+		# setup route for qBittorrent webui using set-mark to route traffic for port 8080 and 8999 to "${docker_interface}"
+		echo "8080    webui" >> /etc/iproute2/rt_tables
+		echo "8999    webui" >> /etc/iproute2/rt_tables
+		ip rule add fwmark 1 table webui
+		ip route add default via "${DEFAULT_GATEWAY}" table webui
+
+		# add mark for traffic on port 8080 (used by the web interface)
+		iptables -t mangle -A OUTPUT -p tcp --dport 8080 -j MARK --set-mark 1
+		iptables -t mangle -A OUTPUT -p tcp --sport 8080 -j MARK --set-mark 1
+	fi
+elif [[ $SET_FWMARK == "yes" ]]; then
+	echo "[ERROR] SET_FWMARK is set to 'yes' but no iptable_mangle support detected."
+	sleep 10
+	exit 1
+fi
+
+if [[ "${DEBUG}" == "yes" ]]; then
+	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] 'main' routing table defined as follows..."
+	echo "--------------------"
+	ip route show table main
+	echo "--------------------"
+	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] ip rules defined as follows..."
+	echo "--------------------"
+	ip rule
+	echo "--------------------"
 fi
 
 # input iptable rules
@@ -142,13 +167,6 @@ iptables -A OUTPUT -s "${docker_network_cidr}" -d "${docker_network_cidr}" -j AC
 # accept output from vpn gateway
 iptables -A OUTPUT -o "${docker_interface}" -p "$VPN_PROTOCOL" --dport "$VPN_PORT" -j ACCEPT
 
-# if iptable mangle is available (kernel module) then use mark
-if [[ $iptable_mangle_exit_code == 0 ]]; then
-	# accept output from qBittorrent webui port - used for external access
-	iptables -t mangle -A OUTPUT -p tcp --dport 8080 -j MARK --set-mark 1
-	iptables -t mangle -A OUTPUT -p tcp --sport 8080 -j MARK --set-mark 1
-fi
-
 # accept output from qBittorrent webui port - used for lan access
 iptables -A OUTPUT -o "${docker_interface}" -p tcp --dport 8080 -j ACCEPT
 iptables -A OUTPUT -o "${docker_interface}" -p tcp --sport 8080 -j ACCEPT
@@ -179,9 +197,16 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 # accept output from local loopback adapter
 iptables -A OUTPUT -o lo -j ACCEPT
 
-echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] iptables defined as follows..."
-echo "--------------------"
-iptables -S
-echo "--------------------"
+if [[ "${DEBUG}" == "yes" ]]; then
+	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] iptables table 'filter' defined as follows..."
+	echo "--------------------"
+	iptables -S -t filter
+	echo "--------------------"
+
+	echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] iptables table 'mangle' defined as follows..."
+	echo "--------------------"
+	iptables -S -t mangle
+	echo "--------------------"
+fi
 
 exec /bin/bash /init/qbittorrent.sh
