345e1d6d7911dec15acf89cb0570cc02e9755a97
Trigus42 <59501676+Trigus42@users.noreply.github.com>
2024-01-07 16:44:06 +0100

    Fix IPv6 not working without privileged mode

--- rootfs/etc/cont-init.d/20-vpn.sh
+++ rootfs/etc/cont-init.d/20-vpn.sh
@@ -180,7 +180,8 @@ if [[ "${VPN_TYPE}" == "wireguard" ]]; then
         # Check if net.ipv4.conf.all.src_valid_mark is already set to 1
         if [ "$(sysctl net.ipv4.conf.all.src_valid_mark)" == "net.ipv4.conf.all.src_valid_mark = 1" ]; then
             # Modify wg-quick to run in unprivileged container
-            sed -i -E 's/&& cmd sysctl -q net.ipv4.conf.all.src_valid_mark=1//gm' "$(command -v wg-quick)"
+            # shellcheck disable=SC2016
+            sed -i -E 's/\[\[ \$proto == -4 ]] && cmd sysctl -q net.ipv4.conf.all.src_valid_mark=1//gm' "$(command -v wg-quick)"
         else
             echo "$(date +'%Y-%m-%d %H:%M:%S') [ERROR] Trying to run in unprivileged mode but $(sysctl net.ipv4.conf.all.src_valid_mark)"
             sleep 5
